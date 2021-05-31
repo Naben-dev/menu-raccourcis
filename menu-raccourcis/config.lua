45adff847700fbe9e1da9f12b24a2a229107a660
Naben <76815289+Naben-dev@users.noreply.github.com>
2021-05-31 22:44:05 +0200

    add files
    add files

---
+++ menu-raccourcis/config.lua
@@ -0,0 +1,195 @@
+-- Menu configuration, array of menus to display
+menuConfigs = {
+    ['emotes'] = {                                  -- Example menu for emotes when player is on foot
+        enableMenu = function()                     -- Function to enable/disable menu handling
+            local player = GetPlayerPed(-1)
+            return IsPedOnFoot(player)
+        end,
+        data = {                                    -- Data that is passed to Javascript
+            keybind = "F9",                         -- Wheel keybind to use (case sensitive, must match entry in keybindControls table)
+            style = {                               -- Wheel style settings
+                sizePx = 600,                       -- Wheel size in pixels
+                slices = {                          -- Slice style settings
+                    default = { ['fill'] = '#000000', ['stroke'] = '#000000', ['stroke-width'] = 2, ['opacity'] = 0.60 },
+                    hover = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 2, ['opacity'] = 0.80 },
+                    selected = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 2, ['opacity'] = 0.80 }
+                },
+                titles = {                          -- Text style settings
+                    default = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    hover = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    selected = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' }
+                },
+                icons = {
+                    width = 64,
+                    height = 64
+                }
+            },
+            wheels = {                              -- Array of wheels to display
+                {
+                    navAngle = 270,                 -- Oritentation of wheel
+                    minRadiusPercent = 0.3,         -- Minimum radius of wheel in percentage
+                    maxRadiusPercent = 0.6,         -- Maximum radius of wheel in percentage
+                    labels = {"Fermer", "Animations", "Porter"},
+                    commands = {"closemenu", "submenu1", "carry"}
+                },
+                {
+                    navAngle = 285,                 -- Oritentation of wheel
+                    minRadiusPercent = 0.6,         -- Minimum radius of wheel in percentage
+                    maxRadiusPercent = 0.9,         -- Maximum radius of wheel in percentage
+                    labels = {"Fermer", "Stopper Anim", "FINGER", "PEACE", "FACEPALM", "DAMN", "FAIL", "DEAD", "GANG1", "GANG2", "COP", "HOLSTER", "CROWDS"},
+                    commands = {"closemenu", "e cancel", "e finger", "e peace", "e palm", "e damn", "e fail", "e dead", "e gang1", "e gang2", "e copidle", "e holster", "e copcrowd2"}
+                }
+            }
+        }
+    },
+    ['vehicles'] = {                                -- Example menu for vehicle controls when player is in a vehicle
+        enableMenu = function()                     -- Function to enable/disable menu handling
+            local player = GetPlayerPed(-1)
+            return IsPedInAnyVehicle(player, false)
+        end,
+        data = {                                    -- Data that is passed to Javascript
+            keybind = "F9",                         -- Wheel keybind to use (case sensitive, must match entry in keybindControls table)
+            style = {                               -- Wheel style settings
+                sizePx = 400,                       -- Wheel size in pixels
+                slices = {                          -- Slice style settings
+                    default = { ['fill'] = '#000000', ['stroke'] = '#000000', ['stroke-width'] = 3, ['opacity'] = 0.60 },
+                    hover = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 3, ['opacity'] = 0.80 },
+                    selected = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 3, ['opacity'] = 0.80 }
+                },
+                titles = {                          -- Text style settings
+                    default = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    hover = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    selected = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' }
+                },
+                icons = {
+                    width = 64,
+                    height = 64
+                }
+            },
+            wheels = {                              -- Array of wheels to display
+                {
+                    navAngle = 270,                 -- Oritentation of wheel
+                    minRadiusPercent = 0.4,         -- Minimum radius of wheel in percentage
+                    maxRadiusPercent = 0.9,         -- Maximum radius of wheel in percentage
+                    labels = {"Fermer", "imgsrc:engine.png", "imgsrc:key.png", "imgsrc:doors.png", "imgsrc:trunk.png", "imgsrc:hood.png"},
+                    commands = {"closemenu", "engine", "lock", "rdoors", "trunk", "hood"}
+                }
+            }
+        }
+    }
+}
+
+-- Menu configuration, array of menus to display
+menuConfigs = {
+    ['menu'] = {
+        enableMenu = function()
+            return true
+        end,
+        data = {
+            keybind = "F9",
+            style = {
+                sizePx = 600,
+                slices = {
+                    default = { ['fill'] = '#000000', ['stroke'] = '#000000', ['stroke-width'] = 3, ['opacity'] = 0.60 },
+                    hover = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 3, ['opacity'] = 0.80 },
+                    selected = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 3, ['opacity'] = 0.80 }
+                },
+                titles = {
+                    default = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    hover = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    selected = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' }
+                },
+                icons = {
+                    width = 64,
+                    height = 64
+                }
+            },
+            wheels = {
+                {
+                    navAngle = 270,
+                    minRadiusPercent = 0.25,
+                    maxRadiusPercent = 0.55,
+                    labels = {"Animations", "Porter", "Objets", "Radio", "Fermer"},
+                    commands = {"submenu1", "carry", "submenu2", "radio", "closemenu"}
+                }
+            }
+        }
+    }
+}
+
+-- Submenu configuration
+subMenuConfigs = {
+    ['submenu2'] = {
+        data = {
+            keybind = "F9",
+            style = {
+                sizePx = 600,
+                slices = {
+                    default = { ['fill'] = '#000000', ['stroke'] = '#000000', ['stroke-width'] = 3, ['opacity'] = 0.60 },
+                    hover = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 3, ['opacity'] = 0.80 },
+                    selected = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 3, ['opacity'] = 0.80 }
+                },
+                titles = {
+                    default = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    hover = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    selected = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' }
+                },
+                icons = {
+                    width = 64,
+                    height = 64
+                }
+            },
+            wheels = {
+                {
+                    navAngle = 270,
+                    minRadiusPercent = 0.25,
+                    maxRadiusPercent = 0.55,
+                    labels = {"Fermer", "Enlever Objet", "Cigare", "Malette", "Fumer", "Joint", "Café", "Carnet Note", "Boite"},
+                    commands = {"closemenu", "e stuckprop", "e cigar", "e suitcase2", "e smoke", "e joint", "e coffee", "e notepad", "e box"}
+                }
+            }
+        }
+    },
+    ['submenu1'] = {
+        data = {
+            keybind = "F7",
+            style = {
+                sizePx = 600,
+                slices = {
+                    default = { ['fill'] = '#000000', ['stroke'] = '#000000', ['stroke-width'] = 3, ['opacity'] = 0.60 },
+                    hover = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 3, ['opacity'] = 0.80 },
+                    selected = { ['fill'] = '#ff8000', ['stroke'] = '#000000', ['stroke-width'] = 3, ['opacity'] = 0.80 }
+                },
+                titles = {
+                    default = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    hover = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' },
+                    selected = { ['fill'] = '#ffffff', ['stroke'] = 'none', ['font'] = 'Helvetica', ['font-size'] = 16, ['font-weight'] = 'bold' }
+                },
+                icons = {
+                    width = 64,
+                    height = 64
+                }
+            },
+            wheels = {
+                {
+                    navAngle = 270,
+                    minRadiusPercent = 0.25,
+                    maxRadiusPercent = 0.55,
+                    labels = {"Fermer", "Stopper Anim", "Contre un mur", "Guarde", "Claquer mains", "Se rendre"},
+                    commands = {"closemenu", "e stop", "e lean", "e guard", "e cheer", "e surrender"}
+                },
+                {
+                    navAngle = 288,
+                    minRadiusPercent = 0.6,
+                    maxRadiusPercent = 0.9,
+                    labels = {"Fermer", "Salut", "Doigt", "Mains holster", "Pompes", "Abdos", "S'assoir", "Décédé", "Croiser Bras", "Policier"},
+                    commands = {"closemenu", "e salute", "e finger", "e reaching", "e pushup", "e situp", "e sitchair", "e cloudgaze", "e crossarms5", "e cop"}
+                }
+            }
+        }
+    },
+}
+
+---------------------------------
+--------- ikNox#6088 ------------
+---------------------------------
